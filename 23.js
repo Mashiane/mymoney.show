b8a77cf5bc62a47e5ec358d51df7f0befd1c9271
Mashiane <anele@mbangas.com>
2013-10-31 15:18:06 -0700

    Fixed alignment of columns in grid

--- 23.js
+++ 23.js
@@ -1,4 +1,4 @@
 
-WhatToDo.innerHTML=[  NSB.List_jqm("lstWhatToDo", "ul", "none", "c", "b", 'My Categories,My Institutions,My Accounts,My Payments,My Reports,Read Me First,Save & Close', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
+WhatToDo.innerHTML=[  NSB.List_jqm("lstWhatToDo", "ul", "none", "c", "b", 'My Categories,My Institutions,My Accounts,My Payments,My Reports,Read Me First', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
   NSB.HeaderBar_jqm("hBar", "MyMoney.Show", "", "false", "", "false", " data-theme=c style='' class=' ' "),
   ].join('');
