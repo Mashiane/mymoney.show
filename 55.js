1273e0578a984e7a7998dac3f475ac6f60be47ca
Mashiane <anele@mbangas.com>
2013-11-02 16:06:25 -0700

    Refresh button across app

--- 55.js
+++ 55.js
@@ -1,4 +1,4 @@
 
-AccountReport.innerHTML=[  NSB.HeaderBar_jqm("accReportTitle", "Account Report", "Back", "arrow-l", "", "false", " data-theme=b style='' class=' ' "),
+AccountReport.innerHTML=[  NSB.HeaderBar_jqm("accReportTitle", "Account Report", "Back", "arrow-l", "Refresh", "refresh", " data-theme=b style='' class=' ' "),
   NSB.Grid("grdAccReport", "1", "3", "", "", "Account,Amount,Percentage", "left,right,right", "style=", ""),
   ].join('');
