87345dbe09b6d0ac309e38ddb2e2097a396fe01f
Mashiane <anele@mbangas.com>
2014-01-10 02:23:39 +0200

    new

--- 31.js
+++ 31.js
@@ -1,4 +1,4 @@
 
-WhatToDo.innerHTML=[  NSB.List_jqm("lstWhatToDo", "ul", "none", "b", "b", 'Make a Payment,Financial Needs,Reports,Settings,Sign Out', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
+WhatToDo.innerHTML=[  NSB.List_jqm("lstWhatToDo", "ul", "none", "b", "b", 'Make a Payment,Financial Needs,Reports,Settings,Database,Sign Out', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
   NSB.HeaderBar_jqm("hBar", "MyMoney.Show", "", "false", "About", "info", " data-theme=b style='' class=' ' "),
   ].join('');
