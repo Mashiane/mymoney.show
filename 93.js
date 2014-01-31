b93179929461acfcbaec9ad2ff418b5273950b8b
Mashiane <anele@mbangas.com>
2014-01-31 10:44:22 +0200

    Update

--- 93.js
+++ 93.js
@@ -1,4 +1,4 @@
 
-FinancialNeedsSelector.innerHTML=[  NSB.List_jqm("lstFinancialNeeds", "ul", "none", "b", "b", 'Financial Needs - Percentage,Financial Needs - Amount', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
+FinancialNeedsSelector.innerHTML=[  NSB.List_jqm("lstFinancialNeeds", "ul", "none", "b", "b", 'Percentage,Amount', "", "N,N,N", "style=", 'class=" "', "100%", true, false, false, "arrow-r"),
   NSB.HeaderBar_jqm("finNeedsSelectorT", "Financial Needs", "Back", "arrow-l", "", "false", " data-theme=b style='' class=' ' "),
   ].join('');
