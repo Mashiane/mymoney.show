2162ef1785be2021da18a7ff3811cda307bb2181
Mashiane <anele@mbangas.com>
2014-01-31 12:34:03 +0200

    Stable

--- 35.js
+++ 35.js
@@ -1,4 +1,4 @@
 
-MyReports.innerHTML=[  NSB.List_jqm("myreportsList", "ul", "none", "b", "b", 'Payments > Report,Payments > Per Year,Payments > Per Month,Accounts > Movements,Accounts > Top 10,Accounts > Report,Categories > Movements,Categories > Top 10,Categories > Report,Monthly Summary,Monthly Summary Charts,Monthly Income vs Expenses,Monthly Cost to Income,History Chart,Financial Position,Assets vs Liabilities,Incomes vs Expenses,Financial Report', "", "N,N,N", "style=", 'class=" "', "100%", false, false, false, "arrow-r"),
+MyReports.innerHTML=[  NSB.List_jqm("myreportsList", "ul", "none", "b", "b", 'Monthly Payments Report,Monthly Summary,Account Movements,Accounts Report,Category Movements,Categories Report,Financial Position,Financial Report', "", "N,N,N", "style=", 'class=" "', "100%", false, false, false, "arrow-r"),
   NSB.HeaderBar_jqm("myreportTitle", "Reports", "Home", "home", "", "false", " data-theme=b style='' class=' ' "),
   ].join('');
